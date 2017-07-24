e69d7ed96a218c7cafacd22a62d2eb4ef2b8518c
Greg Garville Jr <gjgarvil@gmail.comgit config --global user.name Greg>
2017-07-24 15:29:30 -0400

    final commit

--- index.js
+++ index.js
@@ -11,7 +11,14 @@ var day5 = moment().isLeapYear()
 console.log(its + chalk.blue(day))
 console.log(its + ' the ' + chalk.magenta(day2) + ' day of the year')
 console.log(its + '' + chalk.cyan(day3) + ' seconds into the day')
-console.log(its + chalk.green(day4) + ' that it is currently daylight savings time')
+
+if (day4 === true) {
+	console.log('It ' + chalk.green('is') + ' During Daylight Savings Time')
+} else {
+	console.log('It ' + chalk.red('is not') + ' Daylight Savings Time...BRRR!')
+}
+
+//console.log(its + chalk.green(day4) + ' that it is currently daylight savings time')
 
 if (day5 === true) {
 	console.log('It ' + chalk.red('is') + ' a leap year.')
