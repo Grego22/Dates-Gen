4cf165e2af50b784d70ef96e34d4753943d85325
Greg Garville Jr <gjgarvil@gmail.comgit config --global user.name Greg>
2017-07-24 15:22:21 -0400

    second commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const chalk = require('chalk')
+var moment = require('moment')
+const its = 'it is '
+
+var day = moment().format('lll')
+var day2 = moment().format('DDDo')
+var day3 = moment().format('s ss')
+var day4 = moment().isDST()
+var day5 = moment().isLeapYear()
+
+console.log(its + chalk.blue(day))
+console.log(its + ' the ' + chalk.magenta(day2) + ' day of the year')
+console.log(its + '' + chalk.cyan(day3) + ' seconds into the day')
+console.log(its + chalk.green(day4) + ' that it is currently daylight savings time')
+
+if (day5 === true) {
+	console.log('It ' + chalk.red('is') + ' a leap year.')
+} else {
+	console.log('It ' + chalk.red('is not') + ' a leap year.')
+}
